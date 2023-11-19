34349cae84c96b666a60ec09d43379234c65b01f
Bwa <40144023+beaesq@users.noreply.github.com>
2023-11-19 18:40:01 +0800

    Make default person data

---
+++ src/assets/defaults.js
@@ -0,0 +1,50 @@
+export const defaultPerson = {
+  firstName: "Jiwoo",
+  lastName: "Kim",
+  title: "Idol",
+  phone: "555-555-5555",
+  address: "Loona Island, South Korea",
+  email: "chuu@loona.com",
+  summary:
+    "Making sure that fluff gets into the owner's eyes hide head under blanket so no one can see the door is opening! how exciting oh, it's you, meh.",
+  experience: [
+    {
+      id: "c4181244-bb98-4326-b443-c90660d903ad",
+      title: "Artist",
+      company: "ATRP",
+      location: "",
+      begin: "2022",
+      end: "",
+      description:
+        "Entertained all Kkotis around the world, met with business partners to establish realistic sales goals, ended global warming.",
+    },
+    {
+      id: "6d7a052b-3efa-4b37-849e-f2f5c94494c1",
+      title: "Member - LOONA",
+      company: "Redacted",
+      location: "Seoul",
+      begin: "2017",
+      end: "2022",
+      description:
+        "Attacked hearts, loved 4 eva, took people hi high, met with business partners to establish realistic sales goals, ended global warming.",
+    },
+  ],
+  education: [
+    {
+      id: "beecc56d-32af-417d-a04d-e485d4b9fe68",
+      degree: "M.Sc. Environmental Science",
+      school: "University of Kkoti",
+      location: "Cheongju, South Korea",
+      begin: "2020",
+      end: "Present",
+    },
+    {
+      id: "4f660abd-29c0-47e6-b873-c49da42902c8",
+      degree: "B.A. Music",
+      school: "Kkoti University",
+      location: "Cheongju, South Korea",
+      begin: "",
+      end: "2020",
+    },
+  ],
+};
