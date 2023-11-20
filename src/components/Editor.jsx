35119fb3c1243c2ea3d53720b0044da7e24836e4
Bwa <40144023+beaesq@users.noreply.github.com>
2023-11-21 00:32:28 +0800

    Add customize tab

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -6,34 +6,120 @@ import Button from "./Button";
 import "../styles/Editor.css";
 import { useState } from "react";
 
+const defaultStyle = {
+  accent: "#E10E27",
+  font: 0,
+  layout: 0,
+};
+
+function EditTab({ person, setPerson }) {
+  return (
+    <>
+      <PersonalInfoSection
+        legend="Personal Information"
+        person={person}
+        setPerson={setPerson}
+      />
+      <ExperienceSection
+        legend="Experience"
+        person={person}
+        setPerson={setPerson}
+      />
+      <EducationSection
+        legend="Education"
+        person={person}
+        setPerson={setPerson}
+      />
+    </>
+  );
+}
+
+function CustomizeTab({ style, setStyle }) {
+  function updateStyle(newStyle) {
+    const root = document.getElementById("root");
+    root.style.setProperty("--accent-color", newStyle.accent);
+
+    const cv = document.querySelector(".cv");
+    switch (newStyle.font) {
+      case 0:
+        cv.style.setProperty("font-family", "var(--sans-serif-font)");
+        break;
+      case 1:
+        cv.style.setProperty("font-family", "var(--serif-font)");
+        break;
+      case 2:
+        cv.style.setProperty("font-family", "var(--mono-font)");
+        break;
+      default:
+        break;
+    }
+  }
+
+  function handleAccentChange(e) {
+    setStyle({ ...style, accent: e.target.value });
+    updateStyle({ ...style, accent: e.target.value });
+  }
+
+  function handleFontChange(value) {
+    setStyle({ ...style, font: value });
+    updateStyle({ ...style, font: value });
+  }
+
+  return (
+    <>
+      <label htmlFor="accent">Accent color</label>
+      <input
+        onChange={handleAccentChange}
+        type="color"
+        id="accent"
+        value={style.accent}
+      />
+      <p>Font style</p>
+      <div>
+        <Button
+          onClick={() => handleFontChange(0)}
+          text="Sans Serif"
+          className="sans"
+          title="Sans Serif"
+          isActive={style.font === 0}
+        />
+        <Button
+          onClick={() => handleFontChange(1)}
+          text="Serif"
+          className="serif"
+          title="Serif"
+          isActive={style.font === 1}
+        />
+        <Button
+          onClick={() => handleFontChange(2)}
+          text="Mono"
+          className="mono"
+          title="Mono"
+          isActive={style.font === 2}
+        />
+      </div>
+    </>
+  );
+}
+
 export default function Editor({ person, setPerson, className }) {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [style, setStyle] = useState(defaultStyle);
 
   function showTab() {
     switch (activeIndex) {
       case 0:
         return (
-          <form>
-            <PersonalInfoSection
-              legend="Personal Information"
-              person={person}
-              setPerson={setPerson}
-            />
-            <ExperienceSection
-              legend="Experience"
-              person={person}
-              setPerson={setPerson}
-            />
-            <EducationSection
-              legend="Education"
-              person={person}
-              setPerson={setPerson}
-            />
+          <form className="tab">
+            <EditTab person={person} setPerson={setPerson} />
           </form>
         );
       case 1:
-        return <p>1</p>;
-
+        return (
+          <form className="tab">
+            <CustomizeTab style={style} setStyle={setStyle} />
+          </form>
+        );
       default:
         break;
     }
@@ -77,4 +163,14 @@ Editor.propTypes = {
   setPerson: PropTypes.func,
   person: PropTypes.object,
   className: PropTypes.string,
+};
+
+EditTab.propTypes = {
+  setPerson: PropTypes.func,
+  person: PropTypes.object,
+};
+
+CustomizeTab.propTypes = {
+  style: PropTypes.object,
+  setStyle: PropTypes.func,
 };
